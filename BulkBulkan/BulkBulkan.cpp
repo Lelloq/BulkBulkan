5ebf9d6685157d7b1d80b1c720792732dbe5fe3d
Gary Tang <46045360+Lelloq@users.noreply.github.com>
2024-11-15 13:04:04 +0000

    cmake

---
+++ BulkBulkan/BulkBulkan.cpp
@@ -0,0 +1,11 @@
+﻿// BulkBulkan.cpp : Defines the entry point for the application.
+//
+
+#include "BulkBulkan.h"
+
+int main()
+{
+	using namespace std;
+	cout << "Hello CMake." << endl;
+	return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # BulkBulkan
+Learning vulkan and cmake
+Plan to make a 3D renderer using vulkan and utilise computer shaders to make ray tracing out of it
